043d10a150f10067afe800e8fdaa68ba9d760ae8
RianneYM <rianne@youngmaverics.com>
2021-11-23 10:36:39 +0100

    test docs

---
+++ docs/run/jaffle_shop/models/staging/stripe/stg_payments.sql
@@ -0,0 +1,12 @@
+
+  create or replace  view RAW.jaffle_shop.stg_payments  as (
+    select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment
+  );
